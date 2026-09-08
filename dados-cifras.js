f29e1bf1e5f923bb1574a5e5f2ddcd22d6783b93
gthailson650 <gthailson650@gmail.com>
2026-09-08 13:15:06 -0300

    Create dados-cifras.js

---
+++ dados-cifras.js
@@ -0,0 +1,34 @@
+const dadosCifras = {
+  "Minha Música": {
+    artista: "Artista de exemplo",
+    tom: "C",
+    linhas: [
+      ["C", "Esta é uma música de demonstração."],
+      ["G", "Aqui podemos colocar o conteúdo autorizado."],
+      ["Am", "Os acordes aparecerão aqui."],
+      ["F", "E o CifraVerse poderá transpor o tom."]
+    ]
+  },
+
+  "Outra Música": {
+    artista: "Outro Artista",
+    tom: "G",
+    linhas: [
+      ["G", "Esta é outra demonstração."],
+      ["D", "Os acordes podem ser alterados."],
+      ["Em", "O sistema reconhece o tom."],
+      ["C", "E permite subir ou baixar."]
+    ]
+  },
+
+  "Nova Música": {
+    artista: "Novo Artista",
+    tom: "D",
+    linhas: [
+      ["D", "Terceira música de demonstração."],
+      ["A", "Preparada para o CifraVerse."],
+      ["Bm", "Com acordes organizados."],
+      ["G", "E pronta para receber conteúdo."]
+    ]
+  }
+};
