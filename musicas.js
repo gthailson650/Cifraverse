b63bb9cb8daffd76ddf7b105558041b1f4cfee1e
gthailson650 <gthailson650@gmail.com>
2026-09-08 11:42:23 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -5,16 +5,25 @@ const musicas = [
     tom: "C",
     cifra: "cifra.html"
   },
+
   {
     nome: "Outra Música",
     artista: "Outro Artista",
     tom: "G",
     cifra: "cifra.html"
   },
+
   {
     nome: "Nova Música",
     artista: "Novo Artista",
     tom: "D",
+    cifra: "cifra.html"
+  },
+
+  {
+    nome: "Música de Teste",
+    artista: "CifraVerse",
+    tom: "Am",
     cifra: "cifra.html"
   }
 ];
