7f9f25a2226bf6f4ad67ed393ec4d0e945eb5c75
gthailson650 <gthailson650@gmail.com>
2026-09-08 18:48:33 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://jqnxnqxdsdutrwuvonnv.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_W6w3YfaBDWjbTSQV8ZpFQA_vqigXS4Q";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
