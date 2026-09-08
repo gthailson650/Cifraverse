f109febc7ca439dff3bbffeea048f3225165573a
gthailson650 <gthailson650@gmail.com>
2026-09-08 20:51:00 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 const SUPABASE_URL = "https://jqnxnqxdsdutrwuvonnv.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_W6w3YfaBDWjbTSQV8ZpFQA_vqigXS4Q";
+const SUPABASE_KEY =
+  "sb_publishable_W6w3YfaBDWjbTSQV8ZpFQA_vqigXS4Q";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
