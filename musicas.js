bb35598467c489778faa18169bffca4985395333
gthailson650 <gthailson650@gmail.com>
2026-09-08 11:04:42 -0300

    Create musicas.js

---
+++ musicas.js
@@ -0,0 +1,20 @@
+const musicas = [
+  {
+    nome: "Minha Música",
+    artista: "Artista de exemplo",
+    tom: "C",
+    cifra: "cifra.html"
+  },
+  {
+    nome: "Outra Música",
+    artista: "Outro Artista",
+    tom: "G",
+    cifra: "cifra.html"
+  },
+  {
+    nome: "Nova Música",
+    artista: "Novo Artista",
+    tom: "D",
+    cifra: "cifra.html"
+  }
+];
